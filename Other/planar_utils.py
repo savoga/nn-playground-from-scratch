7abc6f6384aaa07dd417a17d2fd456fc4d5fc5c1
savoga <savoure.gael@gmail.com>
2021-06-10 21:48:44 +0200

    update code for comparison

--- Other/planar_utils.py
+++ Other/planar_utils.py
@@ -54,8 +54,8 @@ def load_planar_dataset():
     return X, Y
 
 def load_extra_datasets():
-    N = 200
-    noisy_circles = sklearn.datasets.make_circles(n_samples=N, factor=.5, noise=.3)
+    N = 100
+    noisy_circles = sklearn.datasets.make_circles(n_samples=N, factor=.2, noise=0.2, random_state=3)
     noisy_moons = sklearn.datasets.make_moons(n_samples=N)#, noise=.2)
     blobs = sklearn.datasets.make_blobs(n_samples=N, random_state=5, n_features=2, centers=6)
     gaussian_quantiles = sklearn.datasets.make_gaussian_quantiles(mean=None, cov=0.5, n_samples=N, n_features=2, n_classes=2, shuffle=True, random_state=None)
