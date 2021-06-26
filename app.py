5a08401202a9d5a230d6bd918e98c250c9c85174
savoga <savoure.gael@gmail.com>
2021-06-26 18:43:28 +0200

    distinguish data generation and model

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ import dash_bootstrap_components as dbc
 
 import pandas as pd
 import dash_core_components as dcc
+from dash.exceptions import PreventUpdate
 
 import plotly.express as px
 
 import utils as ut
 import model as md
 
+global df_data
 df_data = pd.DataFrame(columns=['x','y','cluster'])
 fig_data = px.scatter(df_data, x="x", y="y")
 
@@ -28,7 +30,7 @@ app.layout = dbc.Container(
             dcc.Markdown('Data generation'),
 
             dcc.RadioItems(
-                id='data-generation',
+                id='radio-data',
                 options=[
                     {'label': 'Blobs', 'value': 'blobs'},
                     {'label': 'Circles', 'value': 'circles'},
@@ -36,6 +38,14 @@ app.layout = dbc.Container(
                     ],
                 value='blobs',
                 labelStyle={'display': 'block'}
+                    ),
+
+            dbc.Button(
+                    "Generate data",
+                    id="button-generate",
+                    color="primary",
+                    block=True,
+                    className="mb-3",
                     ),
 
             html.Hr(),
@@ -54,36 +64,53 @@ app.layout = dbc.Container(
                     className="mb-3",
                     ),
 
+            dcc.Store(id='intermediate-value')
+            # TODO: IT SEEMS DATAFRAMES CANNOT BE STORED
+
             ]
         )
 
 @app.callback(
-     dash.dependencies.Output('graph-data', 'figure'),
-     [dash.dependencies.Input('data-generation', 'value')])
-def generateData(data_pattern):
-    df_data = ut.generateData(data_pattern=data_pattern)
-    df_data['cluster_start'] = 1 # at the beginning, all points belong to the same class
-    fig_data = px.scatter(df_data, x="x", y="y", color='cluster_start')
-    fig_data.update_traces(marker=dict(size=12, line=dict(width=2, color='DarkSlateGrey')))
-    fig_data.update_layout(coloraxis_showscale=False)
-    return fig_data
+     [dash.dependencies.Output('intermediate-value', 'data'),
+      dash.dependencies.Output('graph-data', 'figure')],
+     [dash.dependencies.Input('button-generate', 'n_clicks'),
+       dash.dependencies.Input('button-nn', 'n_clicks'),
+       dash.dependencies.Input('intermediate-value','data')],
+     [dash.dependencies.State('radio-data', 'value')])
+def generateData(button_generate, button_nn, df_data_stored, radio_pattern):
+    if button_generate is None:
+        raise PreventUpdate
+    ctx = dash.callback_context
+    button_id = ctx.triggered[0]['prop_id'].split('.')[0]
+    if button_id=='button-generate':
+        df_data = ut.generateData(data_pattern=radio_pattern)
+        df_data['cluster_start'] = 1 # at the beginning, all points belong to the same class
+        fig_data = px.scatter(df_data, x="x", y="y", color='cluster_start')
+        fig_data.update_traces(marker=dict(size=12, line=dict(width=2, color='DarkSlateGrey')))
+        fig_data.update_layout(coloraxis_showscale=False)
+        return df_data, fig_data
+    elif button_id=='button-nn':
+        print('NN button pressed')
+        df_data = df_data_stored.copy()
+        nn = md.NeuralNetwork(learning_rate=1.2, nodes_hidden=3)
+        y_iterations = nn.train(df_data[['x','y']], df_data['cluster'], 1000)
+        df_data['cluster'] = y_iterations[len(y_iterations)-1]
+        fig_data = px.scatter(df_data, x="x", y="y", color='cluster')
+        fig_data.update_traces(marker=dict(size=12, line=dict(width=2, color='DarkSlateGrey')))
+        fig_data.update_layout(coloraxis_showscale=False)
+        return None, fig_data
 
-'''
-WARNING: impossible to have 2 callbacks updating the same figure
-Option 1: use CallbackGrouper? (couldn't find how to use it...)
-Option 2: use the same callback for both functionalities
-'''
-@app.callback(
-     dash.dependencies.Output('graph-data', 'figure'),
-     [dash.dependencies.Input('button-nn', 'value')])
-def runNeuralNetwork(fig_data):
-    nn = md.NeuralNetwork(learning_rate=1.2, nodes_hidden=3)
-    y_iterations = nn.train(df_data[['x','y']], df_data['cluster'], 1000)
-    df_data['cluster'] = y_iterations[len(y_iterations)-1]
-    fig_data = px.scatter(df_data, x="x", y="y", color='cluster')
-    fig_data.update_traces(marker=dict(size=12, line=dict(width=2, color='DarkSlateGrey')))
-    fig_data.update_layout(coloraxis_showscale=False)
-    return fig_data
+# @app.callback(
+#      dash.dependencies.Output('graph-data', 'figure'),
+#      [dash.dependencies.Input('button-nn', 'value')])
+# def runNeuralNetwork(fig_data):
+#     nn = md.NeuralNetwork(learning_rate=1.2, nodes_hidden=3)
+#     y_iterations = nn.train(df_data[['x','y']], df_data['cluster'], 1000)
+#     df_data['cluster'] = y_iterations[len(y_iterations)-1]
+#     fig_data = px.scatter(df_data, x="x", y="y", color='cluster')
+#     fig_data.update_traces(marker=dict(size=12, line=dict(width=2, color='DarkSlateGrey')))
+#     fig_data.update_layout(coloraxis_showscale=False)
+#     return fig_data
 
 if __name__ == '__main__':
     app.run_server(debug=True)
